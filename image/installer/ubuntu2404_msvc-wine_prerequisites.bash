f72a84e907e5bce01722c5b66740d3e3436ae8b3
I <1091761+wx257osn2@users.noreply.github.com>
2026-07-04 14:58:14 +0900

    po

--- image/installer/ubuntu2404_msvc-wine_prerequisites.bash
+++ image/installer/ubuntu2404_msvc-wine_prerequisites.bash
@@ -14,7 +14,9 @@ cat << EOF > /opt/fontconf/dummy.conf
 EOF
 
 if [[ $(uname -m) == "aarch64" ]]; then
-  add-apt-repository ppa:fex-emu/fex
+  curl -sS "http://keyserver.ubuntu.com:11371/pks/lookup?op=get&search=0xEDB98BFE8A2310DC9C4A376E76DBFEBEA206F5AC" | gpg --dearmor -o /etc/apt/keyrings/fex-emu.gpg
+  echo "deb [signed-by=/etc/apt/keyrings/fex-emu.gpg] https://ppa.launchpadcontent.net/fex-emu/fex/ubuntu resolute main" >> /etc/apt/sources.list.d/fex-emu.list
+  echo "deb-src [signed-by=/etc/apt/keyrings/fex-emu.gpg] https://ppa.launchpadcontent.net/fex-emu/fex/ubuntu resolute main" >> /etc/apt/sources.list.d/fex-emu.list
   apt-get update
   apt-get install -y --no-install-recommends fex-emu-wine
   path=$(dpkg -L fex-emu-wine | grep -m1 'libwow64fex\.dll$')
